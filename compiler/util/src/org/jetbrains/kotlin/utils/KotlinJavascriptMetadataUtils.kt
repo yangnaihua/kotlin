d4ebebb17827fd508b6cbb2b362d0422b04159ce
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-08-02 17:24:26 +0300

    Raise JS metadata version due to incompatible changes in inliner

--- compiler/util/src/org/jetbrains/kotlin/utils/KotlinJavascriptMetadataUtils.kt
+++ compiler/util/src/org/jetbrains/kotlin/utils/KotlinJavascriptMetadataUtils.kt
@@ -33,7 +33,7 @@ class JsMetadataVersion(vararg numbers: Int) : BinaryVersion(*numbers) {
 
     companion object {
         @JvmField
-        val INSTANCE = JsMetadataVersion(1, 0, 1)
+        val INSTANCE = JsMetadataVersion(1, 0, 5)
 
         @JvmField
         val INVALID_VERSION = JsMetadataVersion()
