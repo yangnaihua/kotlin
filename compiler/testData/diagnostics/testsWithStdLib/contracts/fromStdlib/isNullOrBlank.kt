43b64197b09d86b58bca499e4da9652d0f2db3a9
dsavvinov <dsavvinov@gmail.com>
2017-09-15 16:18:05 +0300

    Effects: add diagnostic tests on functions from stdlib
    ==========
    Introduction of EffectSystem: 18/18

---
+++ compiler/testData/diagnostics/testsWithStdLib/contracts/fromStdlib/isNullOrBlank.kt
@@ -0,0 +1,19 @@
+// !LANGUAGE: +ReturnsEffect
+// !DIAGNOSTICS: -INVISIBLE_REFERENCE -INVISIBLE_MEMBER
+
+fun testIsNullOrBlank(x: String?) {
+    if (x.isNullOrBlank()) {
+        x<!UNSAFE_CALL!>.<!>length
+    } else {
+        <!DEBUG_INFO_SMARTCAST!>x<!>.length
+    }
+}
+
+fun testIsNotNullOrBlank(x: String?) {
+    if (!x.isNullOrBlank()) {
+        <!DEBUG_INFO_SMARTCAST!>x<!>.length
+    }
+
+    x<!UNSAFE_CALL!>.<!>length
+}
+
---
+++ compiler/testData/diagnostics/testsWithStdLib/contracts/fromStdlib/isNullOrEmpty.kt
@@ -0,0 +1,19 @@
+// !LANGUAGE: +ReturnsEffect
+// !DIAGNOSTICS: -INVISIBLE_REFERENCE -INVISIBLE_MEMBER
+
+fun testIsNullOrEmpty(x: String?) {
+    if (x.isNullOrEmpty()) {
+        x<!UNSAFE_CALL!>.<!>length
+    } else {
+        <!DEBUG_INFO_SMARTCAST!>x<!>.length
+    }
+}
+
+fun testIsNotNullOrEmpty(x: String?) {
+    if (!x.isNullOrEmpty()) {
+        <!DEBUG_INFO_SMARTCAST!>x<!>.length
+    }
+
+    x<!UNSAFE_CALL!>.<!>length
+}
+
