2bd4e5d1404f44befdc295e494dae8f8725f5373
dsavvinov <dsavvinov@gmail.com>
2017-09-15 13:43:37 +0300

    Effects: add cfg-tests on inlined lambdas
    ==========
    Introduction of Effect System: 15/18

---
+++ compiler/testData/cfgVariablesWithStdLib/contracts/breakContinuesInInlinedLambda.kt
@@ -0,0 +1,45 @@
+// LANGUAGE_VERSION: 1.3
+
+import kotlin.internal.contracts.*
+
+inline fun <T> myRun(block: () -> T): T {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    return block()
+}
+
+fun getBoolean(): Boolean = false
+
+fun getBoolean() = false
+
+fun test() {
+    val x: Int
+
+    if (getBoolean())
+        run {
+            while (getBoolean()) {
+                do {
+                    run {
+                        if (getBoolean()) {
+                            x = 42
+                        }
+                        else {
+                            x = 43
+                        }
+                    }
+                    break
+                } while (getBoolean())
+                run { x.inc() }
+                run { x = 42 }
+                break
+            }
+            x = 42
+        }
+    else
+        run {
+            x = 42
+        }
+
+    x.inc()
+}
